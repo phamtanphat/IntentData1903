ce3610c1bd031649d063c069dce5bb36478d9a09
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-09 20:55:18 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentdata/Person.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentdata/Person.java
@@ -1,6 +1,8 @@
 package khoapham.ptp.phamtanphat.intentdata;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable {
     String ten;
 
     public Person(String ten) {
